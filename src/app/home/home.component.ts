dabfb8f17c5427af4cdc93d60b8bdecc325c8427
salim <sgkapasi@gmail.com>
2021-02-12 18:09:17 -0500

    shareReplay

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {noop, Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {map, shareReplay, tap} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 import {Course} from '../model/course';
 
@@ -23,19 +23,17 @@ export class HomeComponent implements OnInit {
 
     const http$ = createHttpObservable('/api/courses');
 
-    const courses$: Observable<Course[]> = http$.pipe(map(res => Object.values(res['payload'])));
+    const courses$: Observable<Course[]> = http$.pipe(
+      shareReplay(),
+      tap(() => console.log('http called')),
+      map(res => Object.values(res['payload'])),
+    );
 
     this.beginnersCourses$ = courses$.pipe(map(courses => courses
         .filter(course => course.category === 'BEGINNER')));
 
     this.advancedCourses$ = courses$.pipe(map(courses => courses
       .filter(course => course.category === 'ADVANCED')));
-
-    courses$.subscribe(
-      courses => console.log(courses),
-      noop,
-      () => console.log('completed')
-    );
   }
 
 
