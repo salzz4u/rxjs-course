75b2492656d788dc73f2da61e14547058b83c43f
salim <sgkapasi@gmail.com>
2021-02-12 20:50:47 -0500

    updated fetch to cancel request - abortController with signal pattern

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -2,13 +2,19 @@ import {Observable} from 'rxjs';
 
 export function createHttpObservable(url: string): Observable<any> {
   return Observable.create(observer => {
-    fetch(url)
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch(url, {signal})
       .then(res => res.json())
       .then(body => {
         observer.next(body);
         observer.complete();
       })
       .catch(err => observer.error(err));
+
+    return () => controller.abort();
   });
 }
 
